3e99f73b73f2c4c64397d29abb1343c0ed7c2bba
jluis <jorge.moreno@clickonero.com.mx>
2014-11-04 17:24:28 -0600

    change name of database and add table name to domain model

--- src/main/groovy/com/springboot/domain/User.groovy
+++ src/main/groovy/com/springboot/domain/User.groovy
@@ -1,6 +1,7 @@
 package com.springboot.domain
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -9,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Table(name="user_tb")
 public class User {
 
   @Id
